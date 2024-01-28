cac2b5d7491aa28c029db59b3edb409221aac4a5
Quincy Thai <quincythai50@icloud.com>
2024-01-28 11:54:35 -0800

    Complete bootcamp

--- app/components/MerchItem.tsx
+++ app/components/MerchItem.tsx
@@ -1,10 +1,55 @@
-import { Box, Image } from "@chakra-ui/react";
+"use client";
+
+import { Box, Image, Text, Button } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 
 export default function MerchItem({ source }: { source: string }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Box border="gray solid" borderRadius={8}>
+    <Box
+      border="gray solid"
+      borderRadius={8}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      position="relative"
+    >
       <Image src={source} alt="merch photo" />
+      {isHovered && (
+        <Box
+          position="absolute"
+          top="0"
+          bottom="0"
+          left="0"
+          right="0"
+          textAlign="center"
+          background="rgba(217, 155, 255, 0.8)"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+          paddingY={4}
+          transition="background 0.3s ease-in-out"
+        >
+          <Box>
+            <Text fontWeight="bold" fontSize="3xl">
+              Shirt
+            </Text>
+            <Text>Available in S, M, L, XL</Text>
+            <Text>$40.99 USD</Text>
+          </Box>
+          <Button
+            cursor="pointer"
+            backgroundColor="black"
+            fontWeight="bold"
+            color="white"
+            padding={4}
+          >
+            Buy
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }
--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -5,9 +5,24 @@ export default function Socials() {
   return (
     <>
       <Flex gap={2}>
-        <Image src="instagram-svgrepo-com.svg" alt="instagram logo" width={8} />
-        <Image src="twitter-svgrepo-com.svg" alt="twitter" width={8} />
-        <Image src="facebook-svgrepo-com.svg" alt="facebook" width={8} />
+        <Image
+          src="instagram-svgrepo-com.svg"
+          alt="instagram logo"
+          width={8}
+          cursor="pointer"
+        />
+        <Image
+          src="twitter-svgrepo-com.svg"
+          alt="twitter"
+          width={8}
+          cursor="pointer"
+        />
+        <Image
+          src="facebook-svgrepo-com.svg"
+          alt="facebook"
+          width={8}
+          cursor="pointer"
+        />
       </Flex>
     </>
   );
